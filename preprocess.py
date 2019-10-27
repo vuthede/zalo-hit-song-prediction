9e107151dd510641eec93f0082cbc97672e025f7
vuthede <dethevu@gmail.com>
2019-10-27 11:19:22 +0900

    Get some track info and audio-based features

---
+++ preprocess.py
@@ -0,0 +1,32 @@
+import os, subprocess
+
+INPUT_FOLDER = "/data/zalo/hit-song-prediction/train"
+OUTPUT_FOLDER = "/data/zalo/hit-song-prediction/train-wav-samples"
+CONVERT_RATE = "44100.0"
+START_SECOND = "20"
+DURATION = "20"
+def convert_and_sample(src_folder, dst_folder):
+    if not os.path.isdir(src_folder):
+        print("Source folder doesn't exist, continue")
+    if not os.path.isdir(dst_folder): 
+        os.makedirs(dst_folder)
+        
+    fns = [fn for fn in os.listdir(src_folder) if 
+                any(map(fn.endswith, ['.mp3', '.wav', '.amr']))]
+
+    for i, fn in enumerate(fns): 
+        old_fn = os.path.join(src_folder, fn) 
+        new_fn = os.path.join(dst_folder, fn + '.wav')
+        # new_fn = dst_folder + fn + '.wav'
+        if os.path.isfile(new_fn): 
+            print("hahahaha")
+            continue
+        # convert all file to wav, mono, sample rate 8000
+        subprocess.call(['ffmpeg', '-loglevel', 'panic', '-i', old_fn,
+                         '-ss', START_SECOND, '-t', DURATION, 
+                         '-ar', CONVERT_RATE, '-c', 'copy', new_fn])
+        if (i+1)%100 == 0:
+            print('{}/{}: {}'.format(i+1, len(fns), new_fn))
+
+convert_and_sample(INPUT_FOLDER, OUTPUT_FOLDER)
+    
